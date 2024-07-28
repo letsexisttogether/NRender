5d8ca8d6751ad547279a9daedbf4a684f1ec9041
letsexisttogether <letmeexist95@gmail.com>
2024-07-28 22:40:22 +0300

    Remade the way the program treats shaders:

---
+++ Source/Shaders/Shader.cpp
@@ -0,0 +1,56 @@
+#include "Shader.hpp"
+
+#include "Boundable/Boundable.hpp"
+#include "GLEW/glew.h"
+
+Shader::Shader(const Type type, const ShaderSource& source)
+    : Boundable{ type }, m_ShaderSource{ source }
+{
+    Generate();
+}
+
+void Shader::Bind() noexcept
+{
+    glAttachShader(m_ProgramID, m_ID);
+}
+
+void Shader::UnBind() noexcept
+{
+    glAttachShader(m_ProgramID, 0);
+}
+
+void Shader::Compile() noexcept
+{
+    const char* rawSource = m_ShaderSource.data();
+
+    glShaderSource(m_ID, 1, &rawSource, nullptr);
+
+    glCompileShader(m_ID);
+}
+
+const Shader::ShaderSource& Shader::GetShaderSource()
+    const noexcept
+{
+    return m_ShaderSource;
+}
+
+void Shader::SetShaderSource(const ShaderSource& source)
+    noexcept
+{
+    m_ShaderSource = source;
+}
+
+Shader::BoundableID Shader::GetProgramID() const noexcept
+{
+    return m_ProgramID;
+}
+
+void Shader::SetProgramID(const BoundableID programID) noexcept
+{
+    m_ProgramID = programID;
+}
+
+void Shader::Generate() noexcept
+{
+    m_ID = glCreateShader(m_Type);
+}
---
+++ Source/Shaders/Shader.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+
+#include "Boundable/Boundable.hpp"
+
+class Shader : public Boundable
+{
+public:
+    using ShaderSource = std::string;
+
+public:
+    Shader() = delete;
+    Shader(const Shader&) = default;
+    Shader(Shader&&) = default;
+
+    Shader(const Type type, const ShaderSource& source);
+
+    ~Shader() = default;
+
+    void Bind() noexcept override;
+    void UnBind() noexcept override;
+
+    void Compile() noexcept;
+
+    const ShaderSource& GetShaderSource() const noexcept;
+    void SetShaderSource(const ShaderSource& source) noexcept;
+    
+    BoundableID GetProgramID() const noexcept;
+    void SetProgramID(const BoundableID programID) noexcept;
+
+protected:
+    void Generate() noexcept override;
+
+private:
+    ShaderSource m_ShaderSource{};
+    BoundableID m_ProgramID{};
+};
