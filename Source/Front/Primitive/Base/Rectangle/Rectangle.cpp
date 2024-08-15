60160d06767f6156102bc5e44618cf5744dbacdb
letsexisttogether <letmeexist95@gmail.com>
2024-08-15 17:19:31 +0300

    Remoned Rectangle origin, width and height - now it only has bounds

--- Source/Front/Primitive/Base/Rectangle/Rectangle.cpp
+++ Source/Front/Primitive/Base/Rectangle/Rectangle.cpp
@@ -1,80 +1,15 @@
 #include "Rectangle.hpp"
 
-Rectangle::Rectangle(const Size width, const Size height)
-    : m_Width{ width }, m_Height{ height }
+Rectangle::Rectangle(const Position leftBottom, const Position rightTop)
+    : m_LeftBottom{ leftBottom }, m_RightTop{ rightTop }
 {}
 
-Rectangle::Rectangle(const Position origin, const Size width,
-    const Size height)
-    : m_Origin{ origin }, m_Width{ width }, m_Height{ height }
-{}
-
-Rectangle::Position Rectangle::GetOrigin() const noexcept
+Rectangle::Position Rectangle::GetLeftBotton() const noexcept
 {
-    return m_Origin;
+    return m_LeftBottom;
 }
 
-void Rectangle::SetOrigin(const Position positon) noexcept
+Rectangle::Position Rectangle::GetRightTop() const noexcept
 {
-    m_Origin = positon;
-}
-
-Rectangle::Size Rectangle::GetWidth() const noexcept
-{
-    return m_Width;
-}
-
-Rectangle::Size Rectangle::GetHeight() const noexcept
-{
-    return m_Height;
-}
-
-
-bool Rectangle::CheckPointColision(const GML::Vec2f point) const noexcept
-{
-
-}
-
-Rectangle::Boundries Rectangle::CalculateBoundries() const noexcept
-{
-    const GML::Vec2f leftBottom{ GetLeftBottom() };
-    const GML::Vec2f rightTop{ GetRightTop() };
-
-    Boundries boundries 
-    {
-        { leftBottom.X(), rightTop.Y()   }, { 0.0f, 1.0f },
-        { leftBottom.X(), leftBottom.Y() }, { 0.0f, 0.0f },
-        { rightTop.X(),   rightTop.Y()   }, { 1.0f, 1.0f },
-        { rightTop.X(),   leftBottom.Y() }, { 1.0f, 0.0f },
-    };
-
-    return boundries;
-}
-
-GML::Vec2f Rectangle::GetLeftBottom() const noexcept
-{
-    const Size halfWidth = m_Width / 2.0f;
-    const Size halfHeight = m_Height / 2.0f;
-
-    const GML::Vec2f leftBottom
-    { 
-        m_Origin.X() - halfWidth, 
-        m_Origin.Y() - halfHeight 
-    }; 
-    
-    return leftBottom;
-}
-
-GML::Vec2f Rectangle::GetRightTop() const noexcept
-{
-    const Size halfWidth = m_Width / 2.0f;
-    const Size halfHeight = m_Height / 2.0f;
-
-    const GML::Vec2f rightTop
-    { 
-        m_Origin.X() + halfWidth, 
-        m_Origin.Y() + halfHeight 
-    };
-
-    return rightTop;
+    return m_RightTop;
 }
--- Source/Front/Primitive/Base/Rectangle/Rectangle.hpp
+++ Source/Front/Primitive/Base/Rectangle/Rectangle.hpp
@@ -16,30 +16,19 @@ public:
     Rectangle(const Rectangle&) = default;
     Rectangle(Rectangle&&) = default;
 
-    Rectangle(const Size width, const Size height);
-    Rectangle(const Position origin, const Size width, const Size height);
+    Rectangle(const Position leftBottom, const Position rightTop);
 
-    ~Rectangle() = default;
+    virtual ~Rectangle() = default;
 
     Position GetOrigin() const noexcept;
-    void SetOrigin(const Position positon) noexcept;
 
-    Size GetWidth() const noexcept;
-    Size GetHeight() const noexcept;
-
-    bool CheckPointColision(const GML::Vec2f point) const noexcept;
+    Position GetLeftBotton() const noexcept;
+    Position GetRightTop() const noexcept;
 
     Rectangle& operator = (const Rectangle&) noexcept = default;
     Rectangle& operator = (Rectangle&&) noexcept = default;
 
 protected:
-    Boundries CalculateBoundries() const noexcept;
-
-    GML::Vec2f GetLeftBottom() const noexcept;
-    GML::Vec2f GetRightTop() const noexcept;
-
-protected:
-    Position m_Origin{};
-    Size m_Width{};
-    Size m_Height{};
+    Position m_LeftBottom{};
+    Position m_RightTop{};
 };
